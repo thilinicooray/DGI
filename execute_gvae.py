869b3b6c58d1a121bc397f7a4124ac52159af496
thilinicooray <thilinicooray.ucsc@gmail.com>
2020-09-14 12:12:54 +0800

    gave

--- execute_gvae.py
+++ execute_gvae.py
@@ -191,8 +191,9 @@ for _ in range(50):
     pat_steps = 0
     best_acc = torch.zeros(1)
     best_acc = best_acc.cuda()
+    log.train()
     for _ in range(100):
-        log.train()
+
         opt.zero_grad()
 
         logits = log(train_embs)
@@ -201,7 +202,10 @@ for _ in range(50):
         loss.backward()
         opt.step()
 
-    logits = log(test_embs)
+    log.eval()
+
+    with torch.no_grad():
+        logits = log(test_embs)
     preds = torch.argmax(logits, dim=1)
     acc = torch.sum(preds == test_lbls).float() / test_lbls.shape[0]
     accs.append(acc * 100)
